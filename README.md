ad4c4506cfb245233e40b50c3ca8d48d91abf331
ragrag <raggi144711@bue.edu.eg>
2019-01-28 18:46:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# node-multilanguage-quotes
---
+++ app.js
@@ -0,0 +1,23 @@
+const translate = require('@k3rn31p4nic/google-translate-api');
+const fetch = require('node-fetch');
+
+const getQuote = async function getQuote(lang){
+const getQuotePromise = new Promise((resolve,reject)=>{
+     fetch('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.quoteText);
+        translate(data.quoteText, {from: 'en', to: lang}).then(res => {
+            resolve(res.text)
+        }).catch(err => {
+            reject(err);
+        });
+    })
+    .catch(err => reject(err));
+})
+
+const quote = await getQuotePromise;  
+return quote;
+
+}
+module.exports = getQuote;
