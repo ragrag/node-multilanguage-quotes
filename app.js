24f5add938577feda9146eef370400ec5d8488cb
ragrag <raggi144711@bue.edu.eg>
2019-01-28 19:20:59 +0200

    languages

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const getQuotePromise = new Promise((resolve,reject)=>{
      fetch('http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en')
     .then(res => res.json())
     .then(data => {
-        console.log(data.quoteText);
         translate(data.quoteText, {from: 'en', to: lang}).then(res => {
             resolve(res.text)
         }).catch(err => {
@@ -20,4 +19,6 @@ const quote = await getQuotePromise;
 return quote;
 
 }
+
+
 module.exports = getQuote;
